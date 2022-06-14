6480c089f79f3bd388f63690673836781a48c09a
atguigu <shadingguo6@163.com>
2022-06-14 15:20:53 +0800

    first

---
+++ src/main/java/com/atguigu/git/Hello.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @Author Administrator
+ * @Time 2022-06-14-15:18
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
