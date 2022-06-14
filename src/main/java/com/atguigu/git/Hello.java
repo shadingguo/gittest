ca07a5535baa6cac251d1cb2b34a480a8c58d4e9
shadingguo <52541715+shadingguo@users.noreply.github.com>
2022-06-14 21:29:51 +0800

    Update Hello.java

--- src/main/java/com/atguigu/git/Hello.java
+++ src/main/java/com/atguigu/git/Hello.java
@@ -10,5 +10,6 @@ public class Hello {
         System.out.println("hello world1");
         System.out.println("hot");
         System.out.println("hot2");
+        System.out.println("hot3");
     }
 }
