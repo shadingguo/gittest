e8c5c242d4fabaaac628385a11a5c22a0590eeca
atguigu <shadingguo6@163.com>
2022-06-14 15:21:31 +0800

    second

--- src/main/java/com/atguigu/git/Hello.java
+++ src/main/java/com/atguigu/git/Hello.java
@@ -7,5 +7,6 @@ package com.atguigu.git;
 public class Hello {
     public static void main(String[] args) {
         System.out.println("hello world");
+        System.out.println("hello world1");
     }
 }
